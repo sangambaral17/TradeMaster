5eadf3f8ce41e05b0d80e154ea36a64c6c6c6054
sangambaral17 <sangambaral9@gmail.com>
2025-12-04 19:52:30 +0545

    fix: Settings page dropdowns not working - Fixed SettingsView to inject ViewModel and set DataContext - Added TwoWay binding mode to Language dropdown - Added TwoWay binding mode to Currency Symbol dropdown - Dropdowns now properly display and save selections

--- TradeMaster.Desktop/Views/SettingsView.xaml.cs
+++ TradeMaster.Desktop/Views/SettingsView.xaml.cs
@@ -1,12 +1,14 @@
 using System.Windows;
+using TradeMaster.Desktop.ViewModels;
 
 namespace TradeMaster.Desktop.Views
 {
     public partial class SettingsView : Window
     {
-        public SettingsView()
+        public SettingsView(SettingsViewModel viewModel)
         {
             InitializeComponent();
+            DataContext = viewModel;
         }
     }
 }
